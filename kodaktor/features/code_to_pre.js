cbf81fe67efb0b9484ef395987aca7da71c10146
kovdmm <kov.dmm@gmail.com>
2020-03-24 06:02:48 +0300

    Revert "Moved book"
    This reverts commit bfdc9b8b4421bf0c8e078aa058ca327d819d4ae7.

--- kodaktor/features/code_to_pre.js
+++
@@ -1,9 +0,0 @@
-// Access: https://kodaktor.ru/j/code_to_pre_19f77
-(function(window){
-	let code = document.createElement('code');
-	let pre = document.createElement('pre');
-	pre.appendChild(code);
-	code.innerText = (js_beautify(document.querySelector('#code').innerText, { indent_size: 2, space_in_empty_paren: true }));
-	document.body.appendChild(pre);
-	hljs.initHighlightingOnLoad();
-})(this);
