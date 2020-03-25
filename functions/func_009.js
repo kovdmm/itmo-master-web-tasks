39f77f4af96ae8acad4ab8f5da12f90eebda4a66
kovdmm <kov.dmm@gmail.com>
2020-03-25 03:13:07 +0300

    Update comments in func_003 (generators)

---
+++ functions/func_009.js
@@ -0,0 +1,1 @@
+// See: ../generators/gen.js
--- generators/gen.js
+++ generators/gen.js
@@ -1,9 +1,16 @@
+/* 
+ * Task: https://kodaktor.ru/func_009
+ *
+ * Дана функция cc.
+ * Нужно вызвать эту функцию и с помощью ещё трёх вызовов получить подряд идущие значения привязки x: 1, 2, 3
+ *
+ * Info: https://kodaktor.ru/generators_1
+ */
+
 const cc = (x = 0) => () => ++x;
 
 let gen_cc = cc();
-console.log(gen_cc());
-console.log(gen_cc());
-console.log(gen_cc());
 
-// TODO: Проделать то же самое на других языках (PHP, Python, ...)
-// см. https://kodaktor.ru/generators_1
+console.log(gen_cc());
+console.log(gen_cc());
+console.log(gen_cc());
